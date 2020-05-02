88fa70213a77e68a52894d1b8028ab01e03f46b5
nsasaki128 <naoki.robert.sasaki@gmail.com>
2020-05-02 20:30:04 +0900

    Add a tokenizer to allow space characters between tokens

--- main.c
+++ main.c
@@ -1,5 +1,98 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+  TK_RESERVED, // Keyworeds or punctuators
+  TK_NUM,      // Integer literals
+  TK_EOF,      // End-of-file markers
+} TokenKind;
+
+// Token type
+typedef struct Token Token;
+struct Token {
+  TokenKind kind; // Token kind
+  Token *next;    // Next token
+  long val;       // If kind is TK_NUM, its value
+  char *loc;      // Token location
+  int len;        // Token length
+};
+
+// Reports an error and exit.
+static void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// Consumes the current token if it matches `s`.
+static bool equal(Token *tok, char *s) {
+  return strlen(s) == tok->len &&
+         !strncmp(tok->loc, s, tok->len);
+}
+
+// Ensure that the current token is `s`.
+static Token *skip(Token *tok, char *s) {
+  if (!equal(tok, s))
+    error("expected '%s'", s);
+  return tok->next;
+}
+
+// Ensure that the current token is TK_NUM.
+static long get_number(Token *tok) {
+  if (tok->kind != TK_NUM)
+    error("expected a number");
+  return tok->val;
+}
+
+// Create a new token and add it as the next token of `cur`.
+static Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->loc = str;
+  tok->len = len;
+  cur->next = tok;
+  return tok;
+}
+
+// Tokenize `p` and returns new tokens.
+static Token *tokenize(char *p) {
+  Token head = {};
+  Token *cur = &head;
+  while (*p) {
+    // Skip whitespace characters.
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    // Punctuator
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++, 1);
+      continue;
+    }
+
+    // Integer literal
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p, 0);
+      char *q = p;
+      cur->val = strtoul(p, &p, 10);
+      cur->len = p - q;
+      continue;
+    }
+
+    error("invalid token");
+  }
+
+  new_token(TK_EOF, cur, p, 0);
+  return head.next;
+
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,26 +100,24 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char *p = argv[1];
+  Token *tok = tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
+  // The first token must be a number
+  printf("  mov rax, %ld\n", get_number(tok));
+  tok = tok->next;
+  while (tok->kind != TK_EOF) {
+    if (equal(tok, "+")) {
+      printf("  add rax, %ld\n", get_number(tok->next));
+      tok = tok->next->next;
       continue;
     }
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-    fprintf(stderr, "unexpected character: '%c'\n", *p);
-    return 1;
+    tok = skip(tok, "-");
+    printf("  sub rax, %ld\n", get_number(tok));
+    tok = tok->next;
   }
 
   printf("  ret\n");
