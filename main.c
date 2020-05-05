b1491a29761e38e7bdb8c925e3dd95d06500ddf6
nsasaki128 <naoki.robert.sasaki@gmail.com>
2020-05-05 16:06:26 +0900

    Add ==, !=, <= and >= operators

--- main.c
+++ main.c
@@ -90,6 +90,10 @@ static Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
   return tok;
 }
 
+static bool startswith(char *p, char *q) {
+  return strncmp(p, q, strlen(q)) == 0;
+}
+
 // Tokenize `current_input` and returns new tokens.
 static Token *tokenize(void) {
   char *p = current_input;
@@ -103,7 +107,15 @@ static Token *tokenize(void) {
       continue;
     }
 
-    // Punctuators
+    // Multi-letter punctuators
+    if (startswith(p, "==") || startswith(p, "!=") ||
+        startswith(p, "<=") || startswith(p, ">=")) {
+          cur = new_token(TK_RESERVED, cur, p, 2);
+          p += 2;
+          continue;
+        }
+
+    // Single-letter punctuators
     if (ispunct(*p)) {
       cur = new_token(TK_RESERVED, cur, p++, 1);
       continue;
@@ -133,6 +145,10 @@ typedef enum {
   ND_ADD, // +
   ND_SUB, // -
   ND_MUL, // *
+  ND_EQ,  // ==
+  ND_NE,  // !=
+  ND_LT,  // <
+  ND_LE, // <=
   ND_DIV, // /
   ND_NUM, // Integer
 } NodeKind;
@@ -166,14 +182,78 @@ static Node *new_num(long val) {
 }
 
 static Node *expr(Token **rest, Token *tok);
+static Node *equality(Token **rest, Token *tok);
+static Node *relational(Token **rest, Token *tok);
+static Node *add(Token **rest, Token *tok);
 static Node *mul(Token **rest, Token *tok);
 static Node *unary(Token **rest, Token *tok);
 static Node *primary(Token **rest, Token *tok);
 
-// expr = mul ("+" mul | "-" mul)*
+// expr = equality
 static Node *expr(Token **rest, Token *tok) {
-  Node *node = mul(&tok, tok);
+  return equality(rest, tok);
+}
+
+// equality = relational ("==" relational | "!=" relational)*
+static Node *equality(Token **rest, Token *tok) {
+  Node *node = relational(&tok, tok);
   
+  for (;;) {
+    if (equal(tok, "==")) {
+      Node *rhs = relational(&tok, tok->next);
+      node = new_binary(ND_EQ, node, rhs);
+      continue;
+    }
+
+    if (equal(tok, "!=")) {
+      Node *rhs = relational(&tok, tok->next);
+      node = new_binary(ND_NE, node, rhs);
+      continue;
+    }
+
+    *rest = tok;
+    return node;
+  }
+}
+
+// relational = add ("<" add | "<=" add | ">" add | ">=" add)*
+static Node *relational(Token **rest, Token *tok) {
+  Node *node = add(&tok, tok);
+
+  for (;;) {
+    if (equal(tok, "<")) {
+      Node *rhs = add(&tok, tok->next);
+      node = new_binary(ND_LT, node, rhs);
+      continue;
+    }
+
+    if (equal(tok, "<=")) {
+      Node *rhs = add(&tok, tok->next);
+      node = new_binary(ND_LE, node, rhs);
+      continue;
+    }
+
+    if (equal(tok, ">")) {
+      Node *rhs = add(&tok, tok->next);
+      node = new_binary(ND_LT, rhs, node);
+      continue;
+    }
+
+    if (equal(tok, ">=")) {
+      Node *rhs = add(&tok, tok->next);
+      node = new_binary(ND_LE, rhs, node);
+      continue;
+    }
+
+    *rest = tok;
+    return node;
+  }
+}
+
+// add = mul ("+" mul | "-" mul)*
+static Node *add(Token **rest, Token *tok) {
+  Node *node = mul(&tok, tok);
+
   for (;;) {
     if (equal(tok, "+")) {
       Node *rhs = mul(&tok, tok->next);
@@ -217,7 +297,6 @@ static Node *mul(Token **rest, Token *tok) {
 
 // unary = ("+" | "-") unary
 //       | primary
-
 static Node *unary(Token **rest, Token *tok) {
   if (equal(tok, "+"))
     return unary(rest, tok->next);
@@ -228,6 +307,7 @@ static Node *unary(Token **rest, Token *tok) {
   return primary(rest, tok);
 }
 
+// primary = "(" expr ")" | num
 static Node *primary(Token **rest, Token *tok) {
   if (equal(tok, "(")) {
     Node *node = expr(&tok, tok->next);
@@ -239,7 +319,6 @@ static Node *primary(Token **rest, Token *tok) {
   *rest = tok->next;
   return node;
 }
-
 
 // 
 // Code generator
@@ -282,6 +361,26 @@ static void gen_expr(Node *node) {
     printf("  cqo\n");
     printf("  idiv %s\n", rs);
     printf("  mov %s, rax\n", rd);
+    return;
+  case ND_EQ:
+    printf("  cmp %s, %s\n", rd, rs);
+    printf("  sete al\n");
+    printf("  movzb %s, al\n", rd);
+    return;
+  case ND_NE:
+    printf("  cmp %s, %s\n", rd, rs);
+    printf("  setne al\n");
+    printf("  movzb %s, al\n", rd);
+    return;
+  case ND_LT:
+    printf("  cmp %s, %s\n", rd, rs);
+    printf("  setl al\n");
+    printf("  movzb %s, al\n", rd);
+    return;
+  case ND_LE:
+    printf("  cmp %s, %s\n", rd, rs);
+    printf("  setle al\n");
+    printf("  movzb %s, al\n", rd);
     return;
   default:
     error("invalid expression");
