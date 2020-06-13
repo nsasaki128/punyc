9a87f8776fc17cb64c76b41203bdad0e7c02f79c
nsasaki128 <naoki.robert.sasaki@gmail.com>
2020-06-13 17:45:21 +0900

    Make sizeof to accept not only an expression but also a typename
    Previously, zincc's sizeof accepted only an expression, so you
    couldn't write something like `sizeof(int)`. Now it accepts that.

--- parse.c
+++ parse.c
@@ -401,6 +401,29 @@ static Type *declarator(Token **rest, Token *tok, Type *ty) {
   return ty;
 }
 
+// abstract-declarator = "*"* ("(" abstract-declarator ")")? type-suffix
+static Type *abstract_declarator(Token **rest, Token *tok, Type *ty)  {
+  while (equal(tok, "*")) {
+    ty = pointer_to(ty);
+    tok = tok->next;
+  }
+
+  if (equal(tok, "(")) {
+    Type *placeholder = calloc(1, sizeof(Type));
+    Type *new_ty = abstract_declarator(&tok, tok->next, placeholder);
+    tok = skip(tok, ")");
+    *placeholder = *type_suffix(rest, tok, ty);
+    return new_ty;
+  }
+
+  return type_suffix(rest, tok, ty);
+}
+
+// type-name = typespec abstract-declarator
+static Type *typename(Token **rest, Token *tok) {
+  Type *ty = typespec(&tok, tok, NULL);
+  return abstract_declarator(rest, tok, ty);
+}
 // declaration = typespec (declarator ("=" expr)? ("," declarator ("=" expr)?)*)?";"
 static Node *declaration(Token **rest, Token *tok) {
   VarAttr attr = {};
@@ -888,6 +911,7 @@ static Node *func_args(Token **rest, Token *tok) {
 
 // primary = "(" "{" stmt stmt* "}" ")" 
 //         | "(" expr ")" 
+//         | "sizeof" "(" type-name ")"
 //         | "sizeof" unary 
 //         | ident func-args? 
 //         | str 
@@ -912,6 +936,12 @@ static Node *primary(Token **rest, Token *tok) {
     Node *node = expr(&tok, tok->next);
     *rest = skip(tok, ")");
     return node;
+  }
+
+  if (equal(tok, "sizeof") && equal(tok->next, "(") && is_typename(tok->next->next)) {
+    Type *ty = typename(&tok, tok->next->next);
+    *rest = skip(tok, ")");
+    return new_num(size_of(ty), tok);
   }
 
   if (equal(tok, "sizeof")) {
