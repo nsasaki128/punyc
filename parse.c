dfcecccea8969f606361388d3481aa91fe8e36cc
nsasaki128 <naoki.robert.sasaki@gmail.com>
2020-06-20 16:04:02 +0900

    Add pre ++ and --

--- parse.c
+++ parse.c
@@ -915,6 +915,7 @@ static Node *cast(Token **rest, Token *tok) {
 }
 
 // unary = ("+" | "-") cast
+//       | ("++" | "--") unary
 //       | postfix
 static Node *unary(Token **rest, Token *tok) {
   if (equal(tok, "+"))
@@ -928,6 +929,12 @@ static Node *unary(Token **rest, Token *tok) {
 
   if (equal(tok, "*"))
     return new_unary(ND_DEREF, cast(rest, tok->next), tok);
+
+  if (equal(tok, "++"))
+    return to_assign(new_add(unary(rest, tok->next), new_num(1, tok), tok));
+
+  if (equal(tok, "--"))
+    return to_assign(new_sub(unary(rest, tok->next), new_num(1, tok), tok));
 
   return postfix(rest, tok);
 }
