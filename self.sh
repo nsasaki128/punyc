f3654fb6df52d48f2edcc4a43653b7d03194d23a
nsasaki128 <naoki.robert.sasaki@gmail.com>
2020-07-27 21:17:01 +0900

    Add parse.c and codegen.c to the stage2 build script

--- self.sh
+++ self.sh
@@ -23,6 +23,14 @@ int fclose(FILE *fp);
 int feof(FILE *stream);
 static void assert() {}
 int strcmp(char *s1, char *s2);
+int printf(char *fmt, ...);
+int sprintf(char *buf, char *fmt, ...);
+long strlen(char *p);
+int strncmp(char *p, char *q);
+void *memvpy(char *dst, char *src, long n);
+char *strndup(char *p, long n);
+int isspace(int c);
+char *strstr(char *haystack, char *needle);
 EOF
 
     grep -v '^#' punyc.h >> $TMP/$1
@@ -32,6 +40,7 @@ EOF
     sed -i 's/\btrue\b/1/g; s/\bfalse\b/0/g;' $TMP/$1
     sed -i 's/\bNULL\b/0/g' $TMP/$1
     sed -i 's/, \.\.\.//g' $TMP/$1
+    sed -i 's/INT_MAX/2147483647/g' $TMP/$1
 
     ./punyc $TMP/$1 > $TMP/${1%.c}.s
     gcc -c -o $TMP/${1%.c}.o $TMP/${1%.c}.s
@@ -43,8 +52,8 @@ cc() {
 
 punyc main.c
 punyc type.c
-cc parse.c
-cc codegen.c
+punyc parse.c
+punyc codegen.c
 cc tokenize.c
 
 gcc -static -o punyc-stage2 $TMP/*.o
