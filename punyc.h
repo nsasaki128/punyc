d7d111ad84f6c991c68a0bf601588ff18224a019
nsasaki128 <naoki.robert.sasaki@gmail.com>
2020-07-29 20:57:53 +0900

    Allow to call variadic functions

--- punyc.h
+++ punyc.h
@@ -185,6 +185,7 @@ struct Function {
   char *name;
   Var *params;
   bool is_static;
+  bool is_varargs;
 
   Node *node;
   Var *locals;
@@ -238,6 +239,7 @@ struct Type {
   // Function type
   Type *return_ty;
   Type *params;
+  bool is_varargs;
   Type *next;
 };
 
