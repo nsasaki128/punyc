b5afa0858e47118774f69c8e185b6d775d9612c9
nsasaki128 <naoki.robert.sasaki@gmail.com>
2020-08-03 22:06:34 +0900

    Add unsigned integral types

--- parse.c
+++ parse.c
@@ -363,6 +363,7 @@ static Type *typespec(Token **rest, Token *tok, VarAttr *attr) {
     LONG = 1 << 10,
     OTHER = 1 << 12,
     SIGNED = 1 << 13,
+    UNSIGNED = 1 << 14,
   };
 
   Type *ty = ty_int;
@@ -436,6 +437,8 @@ static Type *typespec(Token **rest, Token *tok, VarAttr *attr) {
       counter += LONG;
     else if (equal(tok, "signed"))
       counter |= SIGNED;
+    else if (equal(tok, "unsigned"))
+      counter |= UNSIGNED;
     else
       error_tok(tok, "internal error");
 
@@ -450,16 +453,27 @@ static Type *typespec(Token **rest, Token *tok, VarAttr *attr) {
     case SIGNED + CHAR:
       ty = ty_char;
       break;
+    case UNSIGNED + CHAR:
+      ty = ty_uchar;
+      break;
     case SHORT:
     case SHORT + INT:
     case SIGNED + SHORT:
     case SIGNED + SHORT + INT:
       ty = ty_short;
       break;
+    case UNSIGNED + SHORT:
+    case UNSIGNED + SHORT + INT:
+      ty = ty_ushort;
+      break;
     case INT:
     case SIGNED:
     case SIGNED + INT:
       ty = ty_int;
+      break;
+    case UNSIGNED:
+    case UNSIGNED + INT:
+      ty = ty_uint;
       break;
     case SIGNED + LONG:
     case SIGNED + LONG + INT:
@@ -470,6 +484,12 @@ static Type *typespec(Token **rest, Token *tok, VarAttr *attr) {
     case LONG + LONG:
     case LONG + LONG + INT:
       ty = ty_long;
+      break;
+    case UNSIGNED + LONG:
+    case UNSIGNED + LONG + INT:
+    case UNSIGNED + LONG + LONG:
+    case UNSIGNED + LONG + LONG + INT:
+      ty = ty_ulong;
       break;
     default:
       error_tok(tok, "invaled type");
@@ -970,7 +990,7 @@ static GvarInitializer *gvar_init_string(char *p, int len) {
 static bool is_typename(Token *tok) {
   static char *kw[] = {
     "void", "_Bool", "char", "short", "int", "long", "struct", "union",
-    "typedef", "enum", "static", "extern", "_Alignas", "signed",
+    "typedef", "enum", "static", "extern", "_Alignas", "signed", "unsigned",
   };
 
   for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++) {
--- punyc.h
+++ punyc.h
@@ -222,6 +222,7 @@ struct Type {
   TypeKind kind;
   int size;           // sizeof() value
   int align;          // alignment
+  bool is_unsigned;   // unsigned or signed
   bool is_incomplete; // incomplete type
 
   // Pointer
@@ -260,6 +261,11 @@ extern Type *ty_char;
 extern Type *ty_short;
 extern Type *ty_int;
 extern Type *ty_long;
+
+extern Type *ty_uchar;
+extern Type *ty_ushort;
+extern Type *ty_uint;
+extern Type *ty_ulong;
 
 bool is_integer(Type *ty);
 Type *copy_type(Type *ty);
