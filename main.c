c3c968c9c363fd99457fd571a5a516cc0615a4cd
nsasaki128 <naoki.robert.sasaki@gmail.com>
2020-08-13 21:10:48 +0900

    Add --help option

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include "punyc.h"
 
+static void usage(void) {
+  fprintf(stderr, "punyc [ -I<path> ] <file>\n");
+  exit(1);
+}
+
 // Returns the contents of a given file.
 static char *read_file(char *path) {
   // By convention, read from stdin if a given filename is "-".
@@ -42,12 +47,23 @@ static char *read_file(char *path) {
 }
 
 int main(int argc, char **argv) {
-  if (argc != 2)
-    error("%s: invalid number of arguments\n", argv[0]);
+  char *filename = NULL;
+
+  for (int i = 1; i < argc; i++) {
+    if (!strcmp(argv[i], "--help"))
+      usage();
+
+    if (argv[i][0] == '-' && argv[i][1] != '\0')
+      error("unknown argument: %s", argv[i]);
+
+    filename = argv[i];
+  }
+  if (!filename)
+    error("no input files");
 
   // Tokenize and parse.
-  char *input = read_file(argv[1]);
-  Token *tok = tokenize(argv[1], input);
+  char *input = read_file(filename);
+  Token *tok = tokenize(filename, input);
   Program *prog = parse(tok);
 
   // Assign offsets to local variables.
