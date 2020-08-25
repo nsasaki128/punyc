a89324f66700e3107f1e7295cc102688f07d9de6
nsasaki128 <naoki.robert.sasaki@gmail.com>
2020-08-25 21:29:12 +0900

    Do not expand a token more than once for the same funclike macro

--- tests/tests.c
+++ tests/tests.c
@@ -147,6 +147,8 @@ int (*fnptr(void))(int) {
   return ret3;
 }
 
+int M9(int x) { return x*x; }
+
 int main() {
   assert(0, 0, "0");
   assert(42, 42, "42");
@@ -1156,6 +1158,10 @@ int main() {
 
 #define M8(x,y) x*y
   assert(12, M8((2,3), 4), "M8((2,3), 4)");
+
+#define M9(x) M10(x) * x
+#define M10(x) M9(x) + 3
+  assert(10, M9(2), "M9(2)");
 
   printf("OK\n");
   return 0;
